9799baee7dca3d24d20d2d4bbb9a3c74844e9125
Ruslan Nekhoroshkin <nrvru@yandex.ru>
2016-10-29 14:37:57 +0300

    Добавлена заготовка приложения

---
+++ src/app/Root.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Root() {
+  return (
+    <div className="wrapper">
+      <h1>Flagman</h1>
+    </div>
+  )
+}
+
+export default Root;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Root from './app/Root';
+
+ReactDOM.render(
+  <Root />,
+  document.getElementById('flagman')
+);
